90bc0bb9bf4fe5762534ef9ba723e3292d4ce6e4
benseal <84064320@qq.com>
2016-05-31 23:42:47 +0800

    add getData()

--- ortec/mainwindow.cpp
+++ ortec/mainwindow.cpp
@@ -21,5 +21,13 @@ void MainWindow::on_actionConnect_triggered()
     addr.append("#1");
     ui->mcbConn->dynamicCall("SetAddress(QString)",addr);
     ui->mcbConn->dynamicCall("Open()");
-    ui->textEdit->setText("hello");
+    QVariant vaData = ui->mcbConn->dynamicCall("GetData(Qint64, Qint64)", 0, 100 );
+    QString str = "";
+    QList<QVariant> l = vaData.toList();
+    for (int i = 0; i < l.length(); i++ ){
+        QString tmp = "";
+        tmp.sprintf("%d:%d\n", i, l[i].toInt());
+        str.append(tmp);
+    }
+    ui->textEdit->setText(str);
 }
