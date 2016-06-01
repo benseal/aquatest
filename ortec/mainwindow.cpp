383990f816c6d2e9d30db2ea4bbd2abd1b6cfff5
liang.gao <glee-0413@163.com>
2016-06-02 00:09:11 +0800

    补充修改

--- ortec/mainwindow.cpp
+++ ortec/mainwindow.cpp
@@ -6,28 +6,75 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    m_isDevOpen = false;
+    ui->actionLog->setDisabled(true);
+    m_logDlg = new LogDialog(this);
+
+    ui->actionLog->setStatusTip("Start log");
+    connect(&this->m_dataThread, SIGNAL(TestSignal(QString)), this, SLOT(setStatusBarMessage(QString)));
 }
 
 MainWindow::~MainWindow()
 {
+    if(m_isDevOpen){
+        ui->mcbConn->dynamicCall("Close()");
+        m_isDevOpen = false;
+    }
     delete ui;
+}
+
+void MainWindow::setStatusBarMessage(QString msg){
+    statusBar()->showMessage(msg);
 }
 
 void MainWindow::on_actionConnect_triggered()
 {
     //m_conn.SetAddress("#l");
     //m_conn.Open();
-    QString addr;
-    addr.append("#1");
-    ui->mcbConn->dynamicCall("SetAddress(QString)",addr);
-    ui->mcbConn->dynamicCall("Open()");
-    QVariant vaData = ui->mcbConn->dynamicCall("GetData(Qint64, Qint64)", 0, 100 );
-    QString str = "";
-    QList<QVariant> l = vaData.toList();
-    for (int i = 0; i < l.length(); i++ ){
-        QString tmp = "";
-        tmp.sprintf("%d:%d\n", i, l[i].toInt());
-        str.append(tmp);
+    if(!m_isDevOpen){
+        QString addr;
+        addr.append("#1");
+        ui->mcbConn->dynamicCall("SetAddress(QString)",addr);
+        ui->mcbConn->dynamicCall("Open()");
+        m_isDevOpen = true;
+        ui->actionConnect->setText("disconnect");
+        ui->actionLog->setEnabled(true);
+    }else{
+        ui->mcbConn->dynamicCall("Close()");
+        m_isDevOpen = false;
+        ui->actionConnect->setText("connect");
+        ui->actionLog->setDisabled(true);
     }
-    ui->textEdit->setText(str);
+
+//    QVariant vaData = ui->mcbConn->dynamicCall("GetData(Qint64, Qint64)", 0, 100 );
+//    QString str = "";
+//    QList<QVariant> l = vaData.toList();
+//    for (int i = 0; i < l.length(); i++ ){
+//        QString tmp = "";
+//        tmp.sprintf("%d:%d\n", i, l[i].toInt());
+//        str.append(tmp);
+//    }
+//    ui->textEdit->setText(str);
+}
+
+void MainWindow::on_actionLog_triggered()
+{
+    if(ui->actionLog->isChecked()){
+        if(QDialog::Rejected == m_logDlg->exec()){
+            return;
+        }
+        ui->actionConnect->setDisabled(true);
+        //ui->actionLog->setDisabled(true);
+        ui->actionLog->setStatusTip("Stop log");
+        m_dataThread.setFilename(m_logDlg->getLogName());
+        m_dataThread.stop(false);
+        m_dataThread.start();
+    }else{
+        m_dataThread.stop(true);
+        ui->actionLog->setEnabled(true);
+        ui->actionConnect->setEnabled(true);
+        m_logDlg->clearLogName();
+        ui->actionLog->setStatusTip("Start log");
+
+    }
 }
--- ortec/mainwindow.h
+++ ortec/mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <logdialog.h>
+#include <getdatathread.h>
 namespace Ui {
 class MainWindow;
 }
@@ -14,13 +15,21 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+private slots:
 
 private slots:
     void on_actionConnect_triggered();
 
+    void on_actionLog_triggered();
+    void setStatusBarMessage(QString msg);
+
 private:
     Ui::MainWindow *ui;
     //UMCBILib::UCONN m_conn;
+    bool m_isDevOpen;
+    LogDialog* m_logDlg;
+    QString m_logName;
+    GetDataThread m_dataThread;
 };
 
 #endif // MAINWINDOW_H
